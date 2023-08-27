4a41ca6ea9c92aeb13fac622ebc1c5f5ff78ec93
Mateus Oliveira <mateus91297826@gmail.com>
2023-08-27 12:44:04 -0300

    v1

--- lambda/index.js
+++ lambda/index.js
@@ -38,9 +38,14 @@ const GetSorteioPokemonIntentHandler = {
             const randomPokemonIndex = getRandomPokemonIndex(pokemons.length);
             const randomPokemon = pokemons[randomPokemonIndex];
             const pokemonName = randomPokemon.name;
+            const pokemonDetails = pokemonResponse.data;
+
+            const typeSlot1 = pokemonDetails.types[0].type.name;
+
+            const pokemonResponse = await axios.get(randomPokemon.url);
 
             randomNumber1 = getRandomNumber(0, 100);
-            const speakOutput = `Encontrando pokemon.....Pokemon Encontrado!!! ${pokemonName},,,,,    Chance de captura: ${randomNumber1} porcento. Você gostaria de capturar este Pokémon?`;
+            const speakOutput = `Encontrando pokemon.....Pokemon Encontrado!!! ${pokemonName},,,,,    Chance de captura: ${randomNumber1} porcento. Você gostaria de capturar este Pokémon do tipo ${typeSlot1}?`;
 
             handlerInput.attributesManager.setSessionAttributes({ pokemonName, randomNumber1 });
 
